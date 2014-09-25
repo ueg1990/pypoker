9dbd5ec701471e0cf744ba217fe72dacba765b8d
ueg1990 <uehtesham90@gmail.com>
2014-09-25 16:28:19 -0400

    Add tests for get_player_index

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,6 +7,10 @@ class UtilsTestCase(unittest.TestCase):
     '''
     def setUp(self):
         self.table = Table(50,100,2,10,100,1000)
+        self.player_1 = Player('usman', 1000, self.table)
+        self.player_2 = Player('ehtesham', 1000, self.table)
+        self.player_3 = Player('gul', 1000, self.table)
+        self.table.players = [self.player_1, self.player_2, self.player_3]
 
     def test_get_max_bet(self):
     	self.assertEqual(utils.get_max_bet([100, 20,15,101, 30, 19]), 101)
@@ -16,6 +20,10 @@ class UtilsTestCase(unittest.TestCase):
 
     def test_rank_comparator(self):
         self.assertEqual(utils.rank_comparator(10, 23), 13)
+
+    def test_get_player_index(self):
+    	index = utils.get_player_index(Player('ehtesham', 1000, self.table))
+    	self.assertEqual(index, 1)
 
     def tearDown(self):
         pass
