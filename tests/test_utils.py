f0907c21bcd26ec46367bfbb2777d0757dd95325
ueg1990 <uehtesham90@gmail.com>
2014-09-25 02:53:12 -0400

    Add tests for functions in utils modules

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,6 +8,15 @@ class UtilsTestCase(unittest.TestCase):
     def setUp(self):
         pass
 
+    def test_get_max_bet(self):
+    	self.assertEqual(utils.get_max_bet([100, 20,15,101, 30, 19]), 101)
+
+    def test_rank_kickers(self):
+    	self.assertEqual(utils.rank_kickers('AAAA', 4), 0.8192)
+
+    def test_rank_comparator(self):
+        self.assertEqual(utils.rank_comparator(10, 23), 13)
+
     def tearDown(self):
         pass
 
