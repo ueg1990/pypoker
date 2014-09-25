7c56d647184f0f24b02820319e879d87191143c9
ueg1990 <uehtesham90@gmail.com>
2014-09-25 00:15:26 -0400

    Add tests for table and player

--- tests/test_player.py
+++ tests/test_player.py
@@ -1,15 +1,19 @@
 import unittest
 from pypoker import Player
+from pypoker import Table
 
 class PlayerTestCase(unittest.TestCase):
     ''' 
     Tests for the Player class
-    ''' 
+    '''
     def setUp(self):
+        self.player = Player('usman', 1000, None)
+
+    def test_table_initialization(self):
+    	self.assertEqual([self.player.player_name, self.player.chips], ['usman', 1000])
+    
+    def tearDown(self):
         pass
 
-    def tearDown(self):
-	pass
-
 if __name__ == '__main__':
-    unittest.main()    
+	unittest.main()
--- tests/test_table.py
+++ tests/test_table.py
@@ -6,10 +6,14 @@ class TableTestCase(unittest.TestCase):
     Tests for the Table class
     '''
     def setUp(self):
-        pass
+        self.table = Table(50, 100, 2, 10, 100, 1000)
 
+    def test_table_initialization(self):
+    	self.assertEqual([self.table.small_blind, self.table.big_blind, self.table.min_players,
+    	self.table.max_players, self.table.min_buy_in, self.table.max_buy_in], [50, 100, 2, 10, 100, 1000])
+    
     def tearDown(self):
         pass
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
